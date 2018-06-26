b3704167056acc29dcab52a1c44a94f79510327f
Shaiful Aiman Malik <ibnumalik@tbd.my>
2018-06-26 13:02:43 +0800

    Add a function to enqueue scripts and styles

--- web/app/themes/timber/functions.php
+++ web/app/themes/timber/functions.php
@@ -4,11 +4,11 @@ if ( ! class_exists( 'Timber' ) ) {
 	add_action( 'admin_notices', function() {
 		echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php') ) . '</a></p></div>';
 	});
-	
+
 	add_filter('template_include', function($template) {
 		return get_stylesheet_directory() . '/static/no-timber.html';
 	});
-	
+
 	return;
 }
 
@@ -25,6 +25,7 @@ class StarterSite extends TimberSite {
 		add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
 		add_action( 'init', array( $this, 'register_post_types' ) );
 		add_action( 'init', array( $this, 'register_taxonomies' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'load_static_assets') );
 		parent::__construct();
 	}
 
@@ -55,6 +56,12 @@ class StarterSite extends TimberSite {
 		$twig->addExtension( new Twig_Extension_StringLoader() );
 		$twig->addFilter('myfoo', new Twig_SimpleFilter('myfoo', array($this, 'myfoo')));
 		return $twig;
+	}
+
+	function load_static_assets() {
+		$path = get_template_directory_uri();
+
+		wp_enqueue_script('sites', $path . '/static/site.js' );
 	}
 
 }
