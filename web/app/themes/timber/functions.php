b58645e8bafb0b41ed7ecc3bc8ed1dfa92ceebc9
Shaiful <ibnumalik@tbd.my>
2018-10-21 00:09:08 +0800

    Update functions.php to latest stream

--- web/app/themes/timber/functions.php
+++ web/app/themes/timber/functions.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Timber starter-theme
+ * https://github.com/timber/starter-theme
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since   Timber 0.1
+ */
 
 if ( ! class_exists( 'Timber' ) ) {
 	add_action( 'admin_notices', function() {
@@ -8,40 +16,48 @@ if ( ! class_exists( 'Timber' ) ) {
 	add_filter('template_include', function($template) {
 		return get_stylesheet_directory() . '/static/no-timber.html';
 	});
-
 	return;
 }
-
+/**
+ * Sets the directories (inside your theme) to find .twig files
+ */
 Timber::$dirname = array('templates', 'views');
 
+/**
+ * By default, Timber does NOT autoescape values. Want to enable Twig's autoescape?
+ * No prob! Just set this value to true
+ */
+Timber::$autoescape = false;
+
+/**
+ * We're going to configure our theme inside of a subclass of Timber\Site
+ * You can move this to its own file and include here via php's include("MySite.php")
+ */
 class StarterSite extends TimberSite {
 
 	private $manifest;
-
+	/** Add timber support. */
 	function __construct() {
-
-		add_theme_support( 'post-formats' );
-		add_theme_support( 'post-thumbnails' );
-		add_theme_support( 'menus' );
-		add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
+		add_action( 'after_setup_theme', array( $this, 'theme_supports' ) );
 		add_filter( 'timber_context', array( $this, 'add_to_context' ) );
 		add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
 		add_action( 'init', array( $this, 'register_post_types' ) );
 		add_action( 'init', array( $this, 'register_taxonomies' ) );
+
 		add_action( 'after_setup_theme', array( $this, 'get_manifest' ) );
 		add_action( 'wp_enqueue_scripts', array( $this, 'load_static_assets' ) );
 		parent::__construct();
 	}
 
-	function register_post_types() {
-		//this is where you can register custom post types
+	/* This is where you can register custom post types */
+	public function register_post_types() {
 	}
 
-	function register_taxonomies() {
-		//this is where you can register custom taxonomies
+	/* This is where you can register custom taxonomies */
+	public function register_taxonomies() {
 	}
 
-	function add_to_context( $context ) {
+	public function add_to_context( $context ) {
 		$context['foo'] = 'bar';
 		$context['stuff'] = 'I am a value set in your functions.php file';
 		$context['notes'] = 'These values are available everytime you call Timber::get_context();';
@@ -51,30 +67,82 @@ class StarterSite extends TimberSite {
 		return $context;
 	}
 
-	function myfoo( $text ) {
+	public function theme_supports() {
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support( 'post-thumbnails' );
+
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support(
+			'html5', array(
+				'comment-list',
+				'comment-form',
+				'search-form',
+				'gallery',
+				'caption'
+			)
+		);
+
+		/*
+		 * Enable support for Post Formats.
+		 *
+		 * See: https://codex.wordpress.org/Post_Formats
+		 */
+		add_theme_support(
+			'post-formats', array(
+				'aside',
+				'image',
+				'video',
+				'quote',
+				'link',
+				'gallery',
+				'audio',
+			)
+		);
+		add_theme_support( 'menus' );
+	}
+
+	/**
+	 * This would return 'foo bar!'.
+	 *
+	 * @param string $text being 'foo', then return 'foo bar!'.
+	 * @return string
+	 */
+	public function myfoo( $text ) {
 		$text .= ' bar!';
 		return $text;
 	}
 
-	function add_to_twig( $twig ) {
-		/* this is where you can add your own functions to twig */
+	/**
+	 * This is where you can add your own functions to twig
+	 *
+	 * @param string $twig geht extension
+	 * @return object $twig
+	 */
+	public function add_to_twig( $twig ) {
 		$twig->addExtension( new Twig_Extension_StringLoader() );
 		$twig->addFilter('myfoo', new Twig_SimpleFilter('myfoo', array($this, 'myfoo')));
 		return $twig;
 	}
 
-	function get_manifest() {
+	public function get_manifest() {
 		$manifestFile = get_theme_file_path().'/static/manifest.json';
 
 		$this->manifest = file_exists($manifestFile) ?
 			json_decode(file_get_contents($manifestFile), true) : [];
 	}
 
-	function get_asset($name) {
+	public function get_asset($name) {
 		return isset($this->manifest[$name]) ? $this->manifest[$name] : $name;
 	}
 
-	function load_static_assets() {
+	public function load_static_assets() {
 		$path = get_template_directory_uri().'/';
 
 		wp_enqueue_style('styles', $path . $this->get_asset('main.css') );
